94587faaeef27ad04c60823d5868fa573ca0e770
prash <prashant.nagoorkar@gmail.com>
2024-07-10 15:39:36 -0400

    Add project files.

---
+++ AutofacModuleConstructionIssue/MyModule.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+
+namespace AutofacModuleConstructionIssue
+{
+    public class MyModule : Module
+    {
+        public MyModule(ParameterType1 parameterType1)
+        {
+            Console.WriteLine($"Hello from constructor 1");
+            Console.WriteLine();
+        }
+        public MyModule(ParameterType2 parameterType2)
+        {
+            Console.WriteLine($"Hello from constructor 2");
+            Console.WriteLine();
+        }
+        public MyModule(ParameterType1 parameterType1, ParameterType2 parameterType2)
+        {
+            Console.WriteLine($"Hello from constructor 3");
+            Console.WriteLine();
+        }
+    }
+    public class ParameterType1
+    {
+        public string Str1 { get; set; }
+    }
+
+    public class ParameterType2
+    {
+        public string Str2 { get; set; }
+    }
+}
---
+++ AutofacModuleConstructionIssue/Program.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using Autofac.Configuration;
+using Microsoft.Extensions.Configuration;
+
+var configuration1 = new ConfigurationBuilder().AddJsonFile("settings1.json").Build();
+var configuration2 = new ConfigurationBuilder().AddJsonFile("settings2.json").Build();
+var configuration3 = new ConfigurationBuilder().AddJsonFile("settings3.json").Build();
+
+var containerBuilder = new ContainerBuilder();
+containerBuilder.RegisterModule(new ConfigurationModule(configuration1));
+containerBuilder.RegisterModule(new ConfigurationModule(configuration2));
+containerBuilder.RegisterModule(new ConfigurationModule(configuration3));
+var container = containerBuilder.Build();
+
+Console.ReadLine();
